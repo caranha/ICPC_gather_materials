7bbb198a3b45f82a898d0c5e5ad8fa136efd27a6
Claus Aranha <caranha@gmail.com>
2021-03-13 23:33:04 +0900

    ICPC Gather Town Version 02

---
+++ Sponsor Logos.tsx
@@ -0,0 +1,82 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.2" name="Sponsor Logos" tilewidth="1280" tileheight="850" tilecount="26" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="128" height="32" source="nongit/ICPC_2021_Sponsor/ブースのロゴ/JetBrains 128.png"/>
+ </tile>
+ <tile id="1">
+  <image width="68" height="32" source="nongit/ICPC_2021_Sponsor/ブースのロゴ/JetBrains 68.png"/>
+ </tile>
+ <tile id="2">
+  <image width="68" height="32" source="nongit/ICPC_2021_Sponsor/ブースのロゴ/Huawei 68.png"/>
+ </tile>
+ <tile id="3">
+  <image width="128" height="32" source="nongit/ICPC_2021_Sponsor/ブースのロゴ/Huawei 128.png"/>
+ </tile>
+ <tile id="4">
+  <image width="494" height="178" source="nongit/ICPC_2021_Sponsor/ブースのロゴ/logo  LegalForce.png"/>
+ </tile>
+ <tile id="5">
+  <image width="96" height="21" source="nongit/ICPC_2021_Sponsor/ブースのロゴ/logo いい生活.png"/>
+ </tile>
+ <tile id="6">
+  <image width="668" height="850" source="nongit/ICPC_2021_Sponsor/Logos/sponsor-global-programming-tools-Jetbrains@300 (1).png"/>
+ </tile>
+ <tile id="7">
+  <image width="667" height="850" source="nongit/ICPC_2021_Sponsor/Logos/sponsor-diamond-multi-regional-huawei@300 (1).png"/>
+ </tile>
+ <tile id="8">
+  <image width="160" height="64" source="nongit/ICPC_2021_Sponsor/Logos/PFN_logo_resized.png"/>
+ </tile>
+ <tile id="9">
+  <image width="96" height="21" source="nongit/ICPC_2021_Sponsor/ブースのロゴ/logo いい生活.png"/>
+ </tile>
+ <tile id="10">
+  <image width="360" height="397" source="nongit/sponsor/LegalForce.png"/>
+ </tile>
+ <tile id="11">
+  <image width="160" height="64" source="nongit/sponsor/DeNA_logo_RGB.png"/>
+ </tile>
+ <tile id="12">
+  <image width="160" height="64" source="nongit/sponsor/freee_logo_Company_horizontal_01_color_RGB_05_XL-1.png"/>
+ </tile>
+ <tile id="13">
+  <image width="160" height="64" source="nongit/sponsor/ibmneg_blucmyk.png"/>
+ </tile>
+ <tile id="14">
+  <image width="160" height="64" source="nongit/sponsor/KLab_primary_logo_positive.png"/>
+ </tile>
+ <tile id="15">
+  <image width="160" height="64" source="nongit/sponsor/Lf_logo_new_RGB_tate_350dpi.png"/>
+ </tile>
+ <tile id="16">
+  <image width="160" height="64" source="nongit/sponsor/logo_color_836x272px.png"/>
+ </tile>
+ <tile id="17">
+  <image width="160" height="64" source="nongit/sponsor/logo_RGB-1.png"/>
+ </tile>
+ <tile id="18">
+  <image width="160" height="64" source="nongit/sponsor/mercari_logo_horizontal.png"/>
+ </tile>
+ <tile id="19">
+  <image width="160" height="64" source="nongit/sponsor/ONLogo-JapanVert.png"/>
+ </tile>
+ <tile id="20">
+  <image width="160" height="64" source="nongit/sponsor/PFN_logo_resized.png"/>
+ </tile>
+ <tile id="21">
+  <image width="160" height="64" source="nongit/sponsor/retrieva_logo.png"/>
+ </tile>
+ <tile id="22">
+  <image width="160" height="64" source="nongit/sponsor/iiseikatsu.png"/>
+ </tile>
+ <tile id="23">
+  <image width="160" height="64" source="nongit/sponsor/LightTrans.png"/>
+ </tile>
+ <tile id="24">
+  <image width="1280" height="720" source="nongit/ICPC_2021_Sponsor/ブースのロゴ/logo  On Semiconductor/Webex_20Virtual_20Background_9.jpg"/>
+ </tile>
+ <tile id="25">
+  <image width="145" height="145" source="nongit/ICPC_2021_Sponsor/ブースのロゴ/logo  On Semiconductor/ONlogo.bmp"/>
+ </tile>
+</tileset>
---
+++ gather_chairs_1.3.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.2" name="gather_chairs_1.3" tilewidth="32" tileheight="32" tilecount="800" columns="25">
+ <image source="nongit/mapmaking-master/tilesets/gather_chairs_1.3.png" trans="000000" width="800" height="1024"/>
+</tileset>
